4e0cd6214f83f41d1a827df85514e98ad7ab3dfa
Felipe Crespo Gambade <felipecrespo@pm.me>
2023-11-04 07:32:03 +0100

    Setup Cypress Cloud Integration

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "qpa3cb",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
