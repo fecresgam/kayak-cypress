7694269273a5871347fb0f0acea662503da545d8
Felipe Crespo Gambade <felipecrespo@pm.me>
2023-11-04 08:19:08 +0100

    Adding EsLint

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,7 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
-  projectId: "qpa3cb",
+  projectId: 'qpa3cb',
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
--- cypress/e2e/1-getting-started/todo.cy.js
+++ cypress/e2e/1-getting-started/todo.cy.js
@@ -4,9 +4,9 @@
 describe('home page', () => {
   beforeEach(() => {
     cy.visit('https://www.kayak.es/');
-  })
+  });
 
   it('displays cookies message', () => {
     cy.contains('Usamos cookies').should('be.visible');
-  })
-})
+  });
+});
